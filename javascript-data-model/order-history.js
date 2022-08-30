54205201805ed44451482b5e82056ba78ebdf6ad
Matthew Kweon <kweon.matt@gmail.com>
2022-08-30 17:18:51 +0000

    Completed javascript-data-model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,68 @@
+var orderHistory = [
+  {
+    orderPlaced: new Date('2020-07-03'),
+    totalPrice: 138.93,
+    shipTo: 'JS Masher',
+    orderNum: '113-2883177-2648248',
+    deliveredDate: new Date('2020-07-05'),
+    item: [
+      {
+        itemName: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        returnWindowDate: new Date('2020-08-04'),
+        price: 94.95
+
+      },
+      {
+        itemName: 'The Art of Sql',
+        returnWindowDate: new Date('2020-08-04'),
+        price: 33.99
+      }
+    ]
+  },
+  {
+    orderPlaced: new Date('2020-07-04'),
+    totalPrice: 17.22,
+    shipTo: 'JS Masher',
+    orderNum: '114-2875557-9059409',
+    deliveredDate: new Date('2020-07-07'),
+    deliveryResult: 'Your package was delivered. It was handed directly to a resident.',
+    item: [
+      {
+        itemName: 'Gamecube Controller Adapter. Super Smash Bros Switch GameCube Adapter for Wii U, PC. Support Turbo and Vibration Features. No Driver and No Lag-GameCube Adapter',
+        returnWindowDate: new Date('2020-08-05'),
+        price: 15.98
+      }
+    ]
+  },
+  {
+    orderPlaced: new Date('2020-07-19'),
+    totalPrice: 44.53,
+    shipTo: 'JS Masher',
+    orderNum: '113-9984268-1280257',
+    deliveredDate: new Date('2020-07-20'),
+    deliveryResult: 'Your package was delivered. It was handed directly to a resident.',
+    item: [
+      {
+        itemName: 'The Timeless Way of Building',
+        author: 'Alexander, CHristopher',
+        returnWindowDate: new Date('2020-08-19'),
+        price: 41.33
+      }
+    ]
+  },
+  {
+    orderPlaced: new Date('2020-08-04'),
+    totalPrice: 34.00,
+    shipTo: 'JS Masher',
+    orderNum: '114-3941689-8772232',
+    deliveredDate: new Date('2020-08-08'),
+    item: [
+      {
+        itemName: 'JavaScript for impatient programmers',
+        author: 'Rauschmayer, Dr. Axel',
+        returnWindowDate: new Date('2020-09-07'),
+        price: 31.55
+      }
+    ]
+  }
+];
