33eb8a6ee50160799e17691a69616cddc89041a5
Matthew Kweon <kweon.matt@gmail.com>
2022-09-01 21:56:41 +0000

    revised javascript-loops-practice

--- javascript-loops-practice/get-indexes.js
+++ javascript-loops-practice/get-indexes.js
@@ -4,7 +4,7 @@
 [x] create storage for output array
 [x] determine the length of input array and subtract by 1
 [x] push length number into output array
-[x] subtract length of input array by one and push that to the start of the
+[x] subtract length of input array by one and that to the start of the
 input array
 [x] return output
 */
@@ -13,7 +13,7 @@ function getIndexes(array) {
   var output = [];
   var arrayLen = array.length - 1;
   for (var i = arrayLen; i >= 0; i--) {
-    output.splice(0, 0, i);
+    output.unshift(i);
   }
   return output;
 }
