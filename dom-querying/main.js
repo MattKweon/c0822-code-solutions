9d04246b21a69d3cb68d9cdc1f2802f5b09566f3
Matthew Kweon <kweon.matt@gmail.com>
2022-09-06 16:43:25 +0000

    completed dom-querying exercise

---
+++ dom-querying/main.js
@@ -0,0 +1,19 @@
+console.log('hello, world');
+
+var $heading = document.querySelector('h1');
+console.log($heading);
+console.dir($heading);
+
+var $explanation = document.getElementById('explanation');
+console.log($explanation);
+console.dir($explanation);
+
+var $hint = document.getElementsByClassName('hint');
+console.log($hint);
+console.dir($hint);
+
+var $paragraphs = document.querySelectorAll('p');
+console.log($paragraphs);
+
+var $links = document.querySelectorAll('a');
+console.log($links);
