f99834b46e5d9b8c5896bad6eb5fc6fb6e2fe8d8
Matthew Kweon <kweon.matt@gmail.com>
2022-09-06 23:59:51 +0000

    saving progess

--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -1,6 +1,6 @@
 /* exported numVowels */
 /*
-[] set the an output variabel to the number 0
+[] set the an output variable to the number 0
 [] lower case all characters in input string
 [] loop through the new string
 [] figure out in each character is equal to a vowel
@@ -8,3 +8,14 @@
 [] if not, do nothing
 [] return the output
 */
+
+function numVowels(string) {
+  var output = 0;
+  string.toLowerCase();
+  for (var i = 0; i < string.length; i++) {
+    if ('aeiou'.includes(string[i])) {
+      output++;
+    }
+  }
+  return output;
+}
