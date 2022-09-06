8a7572235d75ec738a7f7aca83e78ebd1b1b4806
Matthew Kweon <kweon.matt@gmail.com>
2022-09-06 22:01:18 +0000

    saving progress

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,18 +1,18 @@
 /* exported capitalizeWords */
 /*
-[] create empty string to store the output
-[] split up the string by each individual word and assign that to a variable
-[] capitalize the first letter of the first word in the array
-[] add that word to the output string
-[] loop to the new array and capitalize the first letter of every other word
-[] add that new string into output string
-[] return output
+[x] create empty string to store the output
+[x] split up the string by each individual word and assign that to a variable
+[x] capitalize the first letter of the first word in the array
+[x] add that word to the output string
+[x] loop to the new array and capitalize the first letter of every other word
+[x] add that new string into output string
+[x] return output
 */
 
 function capitalizeWords(string) {
   var output = '';
   var eachWord = string.split(' ');
-  var firstWord = eachWord[0][0].toUpperCase() + eachWord[0].slice(1);
+  var firstWord = eachWord[0][0].toUpperCase() + eachWord[0].slice(1).toLowerCase();
   output += firstWord;
   for (var i = 1; i < eachWord.length; i++) {
     var firstLetter = ' ';
--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -1,1 +1,10 @@
 /* exported numVowels */
+/*
+[] set the an output variabel to the number 0
+[] lower case all characters in input string
+[] loop through the new string
+[] figure out in each character is equal to a vowel
+[] if so, increment the output varibale by 1
+[] if not, do nothing
+[] return the output
+*/
