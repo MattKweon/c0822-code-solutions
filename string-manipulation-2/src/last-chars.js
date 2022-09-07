1c939b9897d623e6f7605e9f657e24d2a565b6e7
Matthew Kweon <kweon.matt@gmail.com>
2022-09-07 07:12:33 +0000

    completed string-manipulation-2

--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -1,1 +1,19 @@
 /* exported lastChars */
+/*
+[x] if value of length is greater than the length of the input string
+   return string
+[x] if not, set a variable for output
+[x] substract string length by the given length value
+[x] slice the input string from that value to the end
+[x] assign that value to output variable
+[x] return output
+*/
+
+function lastChars(length, string) {
+  if (length > string.length) {
+    return string;
+  } else {
+    var output = string.slice(string.length - length);
+    return output;
+  }
+}
--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -1,19 +1,17 @@
 /* exported numVowels */
 /*
-[] set the an output variable to the number 0
-[] lower case all characters in input string
-[] loop through the new string
-[] figure out in each character is equal to a vowel
-[] if so, increment the output varibale by 1
-[] if not, do nothing
-[] return the output
+[x] set the an output variable to the number 0
+[x] loop through the new string
+[x] figure out in each character is equal to a vowel
+[x] if so, increment the output varibale by 1
+[x] if not, do nothing
+[x] return the output
 */
 
 function numVowels(string) {
   var output = 0;
-  string.toLowerCase();
   for (var i = 0; i < string.length; i++) {
-    if ('aeiou'.includes(string[i])) {
+    if ('aeiouAEIOU'.includes(string[i])) {
       output++;
     }
   }
