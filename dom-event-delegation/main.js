8f19e7e464d737838d47f220e01e1ddf1e8ff2d4
Matthew Kweon <kweon.matt@gmail.com>
2022-09-08 22:19:12 +0000

    completed dom-event-delegation

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -1,13 +1,13 @@
 var $taskList = document.querySelector('.task-list');
-var $taskListItem = document.querySelector('.task-list-item');
 
 function singleClick(event) {
   console.log('event.target:', event.target);
   console.log('event.target.tagName:', event.target.tagName);
-  if (event.target.tagName === 'BUTTON') {
-    // $taskList.closest('.task-list-item');
-    console.log('closest .task-list-item', $taskListItem);
-    $taskListItem.remove();
+  if (event.target.tagName !== 'BUTTON') {
+    return;
   }
+  var $taskListItem = event.target.closest('.task-list-item');
+  console.log('closest .task-list-item', $taskListItem);
+  $taskListItem.remove();
 }
 $taskList.addEventListener('click', singleClick);
