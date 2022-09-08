a482c21d1bff1524bcc19b86a925239561622eb4
Matthew Kweon <kweon.matt@gmail.com>
2022-09-08 20:51:01 +0000

    saving progress

---
+++ javascript-view-swapping/main.js
@@ -0,0 +1,17 @@
+// var $tabContainer = document.querySelector('.tab-container');
+// var $tab = document.querySelectorAll('.tab');
+// var $view = document.querySelectorAll('.view');
+
+// function clickTab(event) {
+//   if (event.target.matches('.tab')) {
+
+//   }
+//   for (var i = 0; i < $tab.length; i++) {
+//     if (event.target.className !== 'tab active') {
+//       event.target.className = 'tab active';
+//     }
+//   }
+//   console.log($tab);
+// }
+
+// $tabContainer.addEventListener('click', clickTab);
