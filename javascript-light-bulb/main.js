a97991e3ab885054494038ef40e5e788249e3d15
Matthew Kweon <kweon.matt@gmail.com>
2022-09-08 15:58:39 +0000

    revised javascript-light-bulb

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,13 +1,16 @@
 var $container = document.querySelector('.container');
 var $lightSwitch = document.querySelector('.light-switch');
+var lightSwitchPos = false;
 
 function lightSwitchFlicks() {
-  if ($container.className === 'container off') {
+  if (lightSwitchPos === false) {
     $container.className = 'container on';
     $lightSwitch.className = 'light-switch on';
+    lightSwitchPos = true;
   } else {
     $container.className = 'container off';
     $lightSwitch.className = 'light-switch off';
+    lightSwitchPos = false;
   }
 }
 $lightSwitch.addEventListener('click', lightSwitchFlicks);
