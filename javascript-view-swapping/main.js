a4552f67513085c82cc4aff86074cb132fd7d976
Matthew Kweon <kweon.matt@gmail.com>
2022-09-09 02:05:27 +0000

    completed-view-swapping

--- javascript-view-swapping/main.js
+++ javascript-view-swapping/main.js
@@ -1,6 +1,6 @@
 var $tabContainer = document.querySelector('.tab-container');
 var $tab = document.querySelectorAll('.tab');
-// var $view = document.querySelectorAll('.view');
+var $view = document.querySelectorAll('.view');
 
 function clickTab(event) {
   if (!event.target.matches('.tab')) {
@@ -11,6 +11,14 @@ function clickTab(event) {
       $tab[i].className = 'tab active';
     } else {
       $tab[i].className = 'tab';
+    }
+  }
+  var viewName = event.target.getAttribute('data-view');
+  for (var j = 0; j < $view.length; j++) {
+    if ($view[j].getAttribute('data-view') !== viewName) {
+      $view[j].className = 'view hidden';
+    } else {
+      $view[j].className = 'view';
     }
   }
 }
