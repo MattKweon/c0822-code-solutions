ceb52e157ec94488b3045eb407308219eb6bf068
Matthew Kweon <kweon.matt@gmail.com>
2022-09-10 21:10:00 +0000

    saving progress

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,23 @@
 /* exported omit */
+/*
+[] create empty object for output
+[] loop through the given object
+[] loop through the keys array
+[] compare the property name of the object to the key name
+[] also compare to see if the value of the object property is defined or not
+[] if not true, add the key pair value to the output object
+[] return output
+*/
+
+function omit(source, keys) {
+  var output = {};
+  for (var key in source) {
+    for (var i = 0; i < keys.length; i++) {
+      if (keys[i] !== key && source[key] !== undefined) {
+        output[key] = source[key];
+      }
+    }
+  }
+  return output;
+}
+// console.log(omit({ foo: 1, bar: 2, baz: 3 }, ['foo', 'baz']));
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,27 +1,21 @@
 /* exported pick */
 /*
-[] create empty object for output
-[] loop through the given object
-[]
+[x] create empty object for output
+[x] loop through the given object
+[x] loop through the keys array
+[x] compare the property name of the object to the key name
+[x] also compare to see if the value of the object property is defined or not
+[x] if true, add the key pair value to the output object
+[x] return output
 */
 
-// function pick(source, keys) {
-//   var output = {};
-//   for (var key in source) {
-//     for (var i = 0; i < keys.length; i++) {
-//       if (keys[i] === key && source[key] !== undefined) {
-//         output[key] = source[key];
-//       }
-//     }
-//   }
-//   return output;
-// }
-
 function pick(source, keys) {
-  var output = Object.assign({}, source);
+  var output = {};
   for (var key in source) {
-    if (!keys.includes(key) || source[key] === undefined) {
-      delete output[key];
+    for (var i = 0; i < keys.length; i++) {
+      if (keys[i] === key && source[key] !== undefined) {
+        output[key] = source[key];
+      }
     }
   }
   return output;
