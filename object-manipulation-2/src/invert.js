36fa33e70a340e386f7b346465c70f2459ac8f24
Matthew Kweon <kweon.matt@gmail.com>
2022-09-11 02:55:49 +0000

    completed object-manipulation-2

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,1 +1,16 @@
 /* exported invert */
+/*
+[x] create empty object for output storage
+[x] loop throught the given object
+[x] name a new name of the property value and assign it the origial property name
+[x] add that new key pair value to the output object
+[x] return object
+*/
+
+function invert(source) {
+  var output = {};
+  for (var key in source) {
+    output[source[key]] = key;
+  }
+  return output;
+}
--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,23 +1,18 @@
 /* exported omit */
 /*
-[] create empty object for output
-[] loop through the given object
-[] loop through the keys array
-[] compare the property name of the object to the key name
-[] also compare to see if the value of the object property is defined or not
-[] if not true, add the key pair value to the output object
-[] return output
+[x] create empty object for output
+[x] loop through the given object
+[x] check to see if the keys array includes the key name
+[x] if not add the key value pair to the output array
+[x] return output
 */
 
 function omit(source, keys) {
   var output = {};
   for (var key in source) {
-    for (var i = 0; i < keys.length; i++) {
-      if (keys[i] !== key && source[key] !== undefined) {
-        output[key] = source[key];
-      }
+    if (!keys.includes(key)) {
+      output[key] = source[key];
     }
   }
   return output;
 }
-// console.log(omit({ foo: 1, bar: 2, baz: 3 }, ['foo', 'baz']));
