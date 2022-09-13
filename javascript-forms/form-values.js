1d233971c649ebff2175f4fab41e05229446cd86
Matthew Kweon <kweon.matt@gmail.com>
2022-09-13 15:59:55 +0000

    revised

--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -2,9 +2,9 @@ var $contactForm = document.querySelector('#contact-form');
 
 function handleSubmit(event) {
   event.preventDefault();
-  var name = $contactForm.elements.email.value;
+  var name = $contactForm.elements.name.value;
   var email = $contactForm.elements.email.value;
-  var message = $contactForm.elements.email.value;
+  var message = $contactForm.elements.message.value;
   var messageData = {
     name,
     email,
