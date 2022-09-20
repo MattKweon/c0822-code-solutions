0cf845827ef41f089634eaf43433704fdc5cd456
Matthew Kweon <kweon.matt@gmail.com>
2022-09-20 23:58:13 +0000

    saving progress

---
+++ javascript-carousel/main.js
@@ -0,0 +1,18 @@
+// var count = 0;
+// var $img = document.querySelectorAll('img');
+//
+// function nextImage() {
+//   count++;
+//   for (var i = 0; i < $img.length; i++) {
+//     if (count < 5) {
+//       if (i !== count && i < 5) {
+//         $img[i].className = 'hidden';
+//         $img[count].className = '';
+//       } else {
+//         $img[0].className = '';
+//         $img[4].className = 'hidden';
+//       }
+//     }
+//   }
+// }
+// var nextImageId = setInterval(nextImage, 3000);
