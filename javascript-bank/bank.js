1986a0ce1d77ddbfef70fbf1e2786da84423510d
Matthew Kweon <kweon.matt@gmail.com>
2022-09-20 20:59:58 +0000

    completed javascript-bank

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -8,8 +8,27 @@ Bank.prototype.openAccount = function (holder, balance) {
   if (Number.isInteger(balance) === false || balance <= 0) {
     return null;
   }
-  // new Account(this.nextAccountNumber, holder);
+  var newAccount = new Account(this.nextAccountNumber, holder);
+  newAccount.deposit(balance);
+  this.accounts.push(newAccount);
   this.nextAccountNumber++;
+  return newAccount.number;
+};
 
-  return this.nextAccountNumber;
+Bank.prototype.getAccount = function (number) {
+  for (var i = 0; i < this.accounts.length; i++) {
+    if (this.accounts[i].number === number) {
+      return this.accounts[i];
+    }
+  }
+  return null;
+};
+
+Bank.prototype.getTotalAssets = function () {
+  var output = 0;
+  for (var i = 0; i < this.accounts.length; i++) {
+    var currentAmount = this.accounts[i].getBalance();
+    output += currentAmount;
+  }
+  return output;
 };
