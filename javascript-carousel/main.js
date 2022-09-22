2072892c30383fa2ec1fe0d730e8a199da810d5c
Matthew Kweon <kweon.matt@gmail.com>
2022-09-22 16:01:20 +0000

    saving progress

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -20,7 +20,7 @@
 //     clearInterval(nextImageId);
 //   }
 // }
-// // nextImagIed = setInterval(nextImage, 3000);
+// nextImagId = setInterval(nextImage, 3000);
 
 // var $buttonLeft = document.querySelector('[data-button="left"]');
 // var $buttonRight = document.querySelector('[data-button="right"]');
@@ -29,33 +29,16 @@
 //   clearInterval(nextImageId);
 //   console.log('button clicked');
 //   console.log(count);
-//   $img[count].className = '';
-//   $dot[count].className = 'fa-solid fs-square';
-// if (count === 0) {
-//   $img[0].className = 'hidden';
-//   $dot[0].className = 'fa-light fa-square';
-//   $img[1].className = '';
-//   $dot[1].className = 'fa-solid fa-square';
-// } else if (count === 1) {
-//   $img[1].className = 'hidden';
-//   $dot[1].className = 'fa-light fa-square';
-//   $img[2].className = '';
-//   $dot[2].className = 'fa-solid fa-square';
-// } else if (count === 2) {
-//   $img[2].className = 'hidden';
-//   $dot[2].className = 'fa-light fa-square';
-//   $img[3].className = '';
-//   $dot[3].className = 'fa-solid fa-square';
-// } else if (count === 3) {
-//   $img[3].className = 'hidden';
-//   $dot[3].className = 'fa-light fa-square';
-//   $img[4].className = '';
-//   $dot[4].className = 'fa-solid fa-square';
-// } else if (count === 4) {
-//   $img[4].className = 'hidden';
-//   $dot[4].className = 'fa-light fa-square';
-//   $img[0].className = '';
-//   $dot[0].className = 'fa-solid fa-square';
+//   if (count === 4) {
+//     count = 0;
+//     $img[4].className = 'hidden';
+//     $dot[4].className = 'fa-light fa-square';
+//     $img[0].className = '';
+//     $dot[0].classname = 'fa-solid fa-square';
+//   } else {
+//     $img[count + 1].className = '';
+//     $dot[count + 1].className = 'fa-solid fa-square';
+//   }
 // }
 
 // $buttonRight.addEventListener('click', newImage);
