89c9a2f24551957d307b83148e536ca4f61711f6
Matthew Kweon <kweon.matt@gmail.com>
2022-09-22 05:47:15 +0000

    saving progress

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,25 +1,20 @@
-// var count = 1;
-// var $img = document.querySelectorAll('img');
+var count = 1;
+var $img = document.querySelectorAll('img');
+var $dot = document.querySelectorAll('[data-dot]');
 
-/*
-[] every interval the image should cycle to the next one
-[] increment the count by 1
-[] change the image class name at count to empty string
-[] loop through the img node list and change all other img class names to 'hidden'
-[] once count reaches 5, reset the count to 0
-*/
-// function nextImage() {
-//   if (count < 5) {
-//     $img[count].className = '';
-//     for (var i = 0; i < $img.length; i++) {
-//       if (count !== i) {
-//         $img[i].className = 'hidden';
-//         console.log(i, count);
-//       }
-//     }
-//     count++;
-//   } else {
-//     count = 0;
-//   }
-// }
-// var nextImageId = setInterval(nextImage, 1000);
+function nextImage() {
+  if (count < 5) {
+    $img[count].className = '';
+    $dot[count].className = 'dot fa-solid fa-square';
+    for (var i = 0; i < $img.length; i++) {
+      if (count !== i) {
+        $img[i].className = 'hidden';
+        $dot[i].className = 'dot fa-light fa-square';
+      }
+    }
+    count++;
+  } else {
+    count = 0;
+  }
+}
+setInterval(nextImage, 3000);
