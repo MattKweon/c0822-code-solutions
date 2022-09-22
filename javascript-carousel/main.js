a21725125469025852c1808eed857613814ae5b0
Matthew Kweon <kweon.matt@gmail.com>
2022-09-22 01:58:38 +0000

    saving progress

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,18 +1,25 @@
-// var count = 0;
+// var count = 1;
 // var $img = document.querySelectorAll('img');
-//
+
+/*
+[] every interval the image should cycle to the next one
+[] increment the count by 1
+[] change the image class name at count to empty string
+[] loop through the img node list and change all other img class names to 'hidden'
+[] once count reaches 5, reset the count to 0
+*/
 // function nextImage() {
-//   count++;
-//   for (var i = 0; i < $img.length; i++) {
-//     if (count < 5) {
-//       if (i !== count && i < 5) {
+//   if (count < 5) {
+//     $img[count].className = '';
+//     for (var i = 0; i < $img.length; i++) {
+//       if (count !== i) {
 //         $img[i].className = 'hidden';
-//         $img[count].className = '';
-//       } else {
-//         $img[0].className = '';
-//         $img[4].className = 'hidden';
+//         console.log(i, count);
 //       }
 //     }
+//     count++;
+//   } else {
+//     count = 0;
 //   }
 // }
-// var nextImageId = setInterval(nextImage, 3000);
+// var nextImageId = setInterval(nextImage, 1000);
