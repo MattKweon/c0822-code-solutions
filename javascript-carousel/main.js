a55e6bdf930b2ffe9b10bb53bdbd0f73abcfe3f3
Matthew Kweon <kweon.matt@gmail.com>
2022-09-23 07:30:12 +0000

    completed javascript-carousel

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -4,6 +4,9 @@ var $dot = document.querySelectorAll('[data-dot]');
 var nextImageId = null;
 
 function nextImage() {
+  if (count === 5) {
+    count = 0;
+  }
   if (count < 5) {
     $img[count].className = '';
     $dot[count].className = 'fa-solid fa-square';
@@ -65,11 +68,87 @@ function changeLeft() {
     $img[count].className = '';
     $dot[count].className = 'fa-solid fa-square';
   }
+  nextImageId = setInterval(nextImage, 3000);
+
 }
 $buttonLeft.addEventListener('click', changeLeft);
 
 var $dotContainer = document.querySelector('.dot-container');
 
-function changeImgAt() {
+function changeImgAt(event) {
+  clearInterval(nextImageId);
+  if (event.target === $dot[0]) {
+    if (count === 0) {
+      $img[0].className = '';
+      $dot[0].className = 'fa-solid fa-square';
+      $img[4].className = 'hidden';
+      $dot[4].className = 'fa-light fa-square';
+    } else {
+      $img[0].className = '';
+      $dot[0].className = 'fa-solid fa-square';
+      $img[count - 1].className = 'hidden';
+      $dot[count - 1].className = 'fa-light fa-square';
+    }
+    count = 1;
+  }
+  if (event.target === $dot[1]) {
+    if (count === 0) {
+      $img[1].className = '';
+      $dot[1].className = 'fa-solid fa-square';
+      $img[0].className = 'hidden';
+      $dot[0].className = 'fa-light fa-square';
+      $img[4].className = 'hidden';
+      $dot[4].className = 'fa-light fa-square';
+    } else {
+      $img[1].className = '';
+      $dot[1].className = 'fa-solid fa-square';
+      $img[count - 1].className = 'hidden';
+      $dot[count - 1].className = 'fa-light fa-square';
+    }
+    count = 2;
+  } if (event.target === $dot[2]) {
+    if (count === 0) {
+      $img[2].className = '';
+      $dot[2].className = 'fa-solid fa-square';
+      $img[4].className = 'hidden';
+      $dot[4].className = 'fa-light fa-square';
+    } else {
+      $img[2].className = '';
+      $dot[2].className = 'fa-solid fa-square';
+      $img[count - 1].className = 'hidden';
+      $dot[count - 1].className = 'fa-light fa-square';
+    }
+    count = 3;
+  }
+  if (event.target === $dot[3]) {
+    if (count === 0) {
+      $img[3].className = '';
+      $dot[3].className = 'fa-solid fa-square';
+      $img[4].className = 'hidden';
+      $dot[4].className = 'fa-light fa-square';
+    } else {
+      $img[3].className = '';
+      $dot[3].className = 'fa-solid fa-square';
+      $img[count - 1].className = 'hidden';
+      $dot[count - 1].className = 'fa-light fa-square';
+    }
+    count = 4;
+  }
+  if (event.target === $dot[4]) {
+    if (count === 0) {
+      $img[4].className = '';
+      $dot[4].className = 'fa-solid fa-square';
+      // $img[4].className = 'hidden';
+      // $dot[4].className = 'fa-light fa-square';
+    } else {
+      $img[4].className = '';
+      $dot[4].className = 'fa-solid fa-square';
+      $img[count - 1].className = 'hidden';
+      $dot[count - 1].className = 'fa-light fa-square';
+      count = 5;
+    }
+  }
+  nextImageId = setInterval(nextImage, 3000);
 }
+
 $dotContainer.addEventListener('click', changeImgAt);
