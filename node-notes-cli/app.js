778095c42f288d87f289f95bdcaf5615a280dc2c
Matthew Kweon <kweon.matt@gmail.com>
2022-10-13 01:32:27 +0000

    Completed node-notes-cli challenge.

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -20,3 +20,31 @@ if (process.argv[2] === 'add') {
     });
   });
 }
+if (process.argv[2] === 'delete') {
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    const output = JSON.parse(data);
+    for (const key in output.notes) {
+      if (key === process.argv[3]) {
+        delete output.notes[key];
+      }
+    }
+    fs.writeFile('data.json', JSON.stringify(output, null, 2), err => {
+      if (err) throw err;
+    });
+  });
+}
+if (process.argv[2] === 'update') {
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    const output = JSON.parse(data);
+    for (const key in output.notes) {
+      if (key === process.argv[3]) {
+        output.notes[key] = process.argv[4];
+      }
+    }
+    fs.writeFile('data.json', JSON.stringify(output, null, 2), err => {
+      if (err) throw err;
+    });
+  });
+}
