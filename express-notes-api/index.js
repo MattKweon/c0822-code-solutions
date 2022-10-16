8c4c50a75c689c738fe2c9cd65283720d5b77019
Matthew Kweon <kweon.matt@gmail.com>
2022-10-16 19:38:03 +0000

    If the client uses an id that is not a positive integer, then they should receive a 400 response containing a JSON object with an error property detailing the problem.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -12,6 +12,22 @@ app.get('/api/notes', (req, res) => {
   res.json(output);
 });
 
+app.get('/api/notes/:id', (req, res) => {
+  const errorMsg = {};
+  if (Math.sign(req.params.id) !== 1) {
+    errorMsg.error = 'id must be a positive integer';
+    res.status(400).json(errorMsg);
+  }
+  // for (const key in data.notes) {
+  // if () {
+  //   const errorMsg = {
+  //     error: `cannot find note with id ${}`;
+  //   }
+  //   res.status(401).json(errorMsg);
+  // }
+  // }
+});
+
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
   console.log('Port 3000 is serving!');
