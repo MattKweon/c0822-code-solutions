4b411e9cf517bba760036103b7ae49c5c475253d
Matthew Kweon <kweon.matt@gmail.com>
2022-10-16 20:01:48 +0000

    If there exists a note with the specified id, then they should receive a 200 response containing a JSON object representing the note with that id.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -4,10 +4,12 @@ const app = express();
 
 const data = require('./data');
 
+const notes = data.notes;
+
 app.get('/api/notes', (req, res) => {
   const output = [];
-  for (const key in data.notes) {
-    output.push(data.notes[key]);
+  for (const key in notes) {
+    output.push(notes[key]);
   }
   res.json(output);
 });
@@ -18,13 +20,14 @@ app.get('/api/notes/:id', (req, res) => {
     errorMsg.error = 'id must be a positive integer';
     res.status(400).json(errorMsg);
   }
-  // for (const key in data.notes) {
-  // if () {
-  //   const errorMsg = {
-  //     error: `cannot find note with id ${}`;
-  //   }
-  //   res.status(401).json(errorMsg);
-  // }
+  for (const key in notes) {
+    if (key === req.params.id) {
+      res.json(notes[key]);
+    }
+  }
+  // if (!notes[req.params.id]) {
+  //   errorMsg.error = `cannot find note with id ${req.params.id}`;
+  //   res.status(404).json(errorMsg);
   // }
 });
 
