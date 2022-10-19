ee4df0afaea9afcf31cbb9f83c09f5367ad2a59f
Matthew Kweon <kweon.matt@gmail.com>
2022-10-19 21:00:04 +0000

    POST /api/grades inserts a new grade into the grades table and returns the created grade. The client should receive an object, not an array. The result could be a 201, 400, or 500.

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -40,3 +40,35 @@ app.listen(3000, () => {
   // eslint-disable-next-line no-console
   console.log('Port 3000 is serving');
 });
+
+app.post('/api/grades', (req, res) => {
+  if (!req.body.name || !req.body.course || !req.body.score ||
+    Number(req.body.score < 0) || Number(req.body.score > 100)) {
+    res.status(400).json({
+      error: 'name, course, and valid score(0-100) must be provided.'
+    });
+    return;
+  }
+
+  const name = req.body.name;
+  const course = req.body.course;
+  const score = req.body.score;
+
+  const sql = `
+    insert into "grades" ("name", "course", "score")
+    values ('${name}', '${course}', '${score}')
+    returning *
+  `;
+
+  db.query(sql)
+    .then(result => {
+      const createdGrade = result.rows[0];
+      res.status(201).json(createdGrade);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error occured.'
+      });
+    });
+});
