439e7abceb92c0b57aacf610b23538eaa5e07da0
Matthew Kweon <kweon.matt@gmail.com>
2022-10-19 23:15:24 +0000

    Completed sgt-back-end challange.

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -22,7 +22,6 @@ app.get('/api/grades', (req, res) => {
            "createdAt"
       from "grades"
   `;
-
   db.query(sql)
     .then(result => {
       const grades = result.rows;
@@ -36,30 +35,22 @@ app.get('/api/grades', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-  // eslint-disable-next-line no-console
-  console.log('Port 3000 is serving');
-});
-
 app.post('/api/grades', (req, res) => {
-  if (!req.body.name || !req.body.course || !req.body.score ||
-    Number(req.body.score < 0) || Number(req.body.score > 100)) {
+  const name = req.body.name;
+  const course = req.body.course;
+  const score = req.body.score;
+  if (!name || !course || !score || Number(score) < 0 ||
+  Number(score) > 100) {
     res.status(400).json({
       error: 'name, course, and valid score(0-100) must be provided.'
     });
     return;
   }
-
-  const name = req.body.name;
-  const course = req.body.course;
-  const score = req.body.score;
-
   const sql = `
     insert into "grades" ("name", "course", "score")
     values ('${name}', '${course}', '${score}')
     returning *
-  `;
-
+    `;
   db.query(sql)
     .then(result => {
       const createdGrade = result.rows[0];
@@ -71,4 +62,87 @@ app.post('/api/grades', (req, res) => {
         error: 'An unexpected error occured.'
       });
     });
+});
+
+app.put('/api/grades/:gradeId', (req, res) => {
+  const gradeId = Number(req.params.gradeId);
+  const name = req.body.name;
+  const course = req.body.course;
+  const score = req.body.score;
+  if (!Number.isInteger(gradeId) || gradeId <= 0) {
+    res.status(400).json({
+      error: 'gradeId must be a positive integer.'
+    });
+    return;
+  } else if (!name || !course || !score || Number(score) < 0 ||
+  Number(score) > 100) {
+    res.status(400).json({
+      error: 'name, course, and valid score(0-100) must be provided.'
+    });
+    return;
+  }
+  const sql = `
+    update "grades"
+       set "name"   = '${name}',
+           "course" = '${course}',
+           "score"  = '${score}'
+     where "gradeId" = $1
+     returning *
+  `;
+  const params = [gradeId];
+  db.query(sql, params)
+    .then(result => {
+      const grade = result.rows[0];
+      if (!grade) {
+        res.status(404).json({
+          error: `Cannot find grade with gradeId ${gradeId}.`
+        });
+      } else {
+        res.json(grade);
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error occurred.'
+      });
+    });
+});
+
+app.delete('/api/grades/:gradeId', (req, res) => {
+  const gradeId = Number(req.params.gradeId);
+  if (!Number.isInteger(gradeId) || gradeId <= 0) {
+    res.status(400).json({
+      error: 'gradeId must be a positive integer.'
+    });
+    return;
+  }
+  const sql = `
+    delete from "grades"
+    where "gradeId" = $1
+    returning *
+  `;
+  const params = [gradeId];
+  db.query(sql, params)
+    .then(result => {
+      const grade = result.rows[0];
+      if (!grade) {
+        res.status(404).json({
+          error: `Cannot find grade with gradeId ${gradeId}.`
+        });
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error occured.'
+      });
+    });
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Port 3000 is serving');
 });
