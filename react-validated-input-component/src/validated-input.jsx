afa32e329c583e878f0eaa1be331b7ced2ed6292
Matthew Kweon <kweon.matt@gmail.com>
2022-10-26 19:24:20 +0000

    Defined and exported ValidatedInput component.

--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -1,9 +1,53 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
+import React from 'react';
 
-// class ValidatedInput extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { }
-//   }
-// }
+class ValidatedInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { password: '' };
+
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+  }
+
+  handlePasswordChange(e) {
+    const val = e.target.value;
+    this.setState({ password: val });
+  }
+
+  render() {
+    return (
+      <div>
+        { this.state.password
+          ? (
+          <div>
+            <form>
+              <label>
+                Password
+                <input
+                  type='password'
+                  id='password'
+                  name='password'
+                  value={ this.handlePasswordChange } />
+              </label>
+            </form>
+          </div>
+            )
+          : (
+          <div>
+            <form>
+              <label>
+                Password
+                <input
+                  type='password'
+                  id='password'
+                  name='password'
+                  value={ this.handlePasswordChange } />
+              </label>
+            </form>
+          </div>
+            )}
+      </div>
+    );
+  }
+}
+
+export default ValidatedInput;
