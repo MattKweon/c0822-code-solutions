a951ed126c74c676003339adbf697b5bdb1dcb7e
Matthew Kweon <kweon.matt@gmail.com>
2022-10-26 21:29:04 +0000

    Completed react-validated-input-component challenge.

--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
+function checkIfPasswordValid(str) {
+  const specialChar = /['!@#$%^&*()']/;
+  if (/\d/.test(str) && /[A-Z]/.test(str) && specialChar.test(str)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 class ValidatedInput extends React.Component {
   constructor(props) {
     super(props);
@@ -40,6 +49,22 @@ class ValidatedInput extends React.Component {
             onChange={ this.handlePasswordChange } />
           <i className="x fa-solid fa-xmark"></i>
           <span className='too-short-msg'>Your password is too short</span>
+        </form>
+      );
+    } else if (checkIfPasswordValid) {
+      return (
+        <form>
+          <label htmlFor='password'>Password</label>
+          <input
+            type='password'
+            id='password'
+            name='password'
+            value={this.state.password}
+            onChange={this.handlePasswordChange} />
+          <i className="x fa-solid fa-xmark"></i>
+          <span className='too-short-msg'>
+            Password must include: a number, a capital letter, and a special character
+          </span>
         </form>
       );
     } else {
