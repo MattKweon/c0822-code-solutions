6a9e0f0b31af126518ff22d92cea8486b1664b1d
Matthew Kweon <kweon.matt@gmail.com>
2022-10-28 22:15:25 +0000

    Text container for topics added to an array of objects.

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -31,3 +31,25 @@ const topicNames = [
   { id: '2', name: 'Cascading Style Sheets' },
   { id: '3', name: 'JavaScript' }
 ];
+
+// const topicTextContent = [
+//   {
+//     id: '1', content: `Hypertext Markup Language (HTML) is the standard markup
+//                        language for creating web pages and wev applications. With
+//                        Cascading Style Sheets (CSS) and JavaScript, it forms a
+//                        triad of cornerstone technologies for the World Wide Web.`
+//   },
+//   {
+//     id: '2', content: `Cascading Style Sheets (CSS) is a style sheet language used
+//                        for describing the presentation of a document written in a
+//                        markup language like HTML. CSS is a cornerstone technology
+//                        of the World Wide Web alongside HTML and JavaScript.`
+//   },
+//   {
+//     id: '3', content: `JavaScript often abbreviated as JS, is a high-level,
+//                        interpreted programing language that conforms to the
+//                        ECMAScript specification. JavaScript has curly-bracket
+//                        syntax, dynamic typing, prototype-based object-oriented,
+//                        and first-class functions.`
+//   }
+// ];
