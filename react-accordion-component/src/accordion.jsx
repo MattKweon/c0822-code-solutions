a5e2bf06cad83e5979b777896e015901d4096ccd
Matthew Kweon <kweon.matt@gmail.com>
2022-11-02 06:41:22 +0000

    Revised react-accordion-component to data array as a prop in index.js Modified solution to only have one property of state.

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -4,8 +4,7 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      onClick: null,
-      isOpen: false
+      onClick: null
     };
 
     this.handleClick = this.handleClick.bind(this);
@@ -13,22 +12,30 @@ export default class App extends React.Component {
 
   handleClick(e) {
     const id = e.target.getAttribute('data-id');
-    this.setState({
-      onClick: id,
-      isOpen: true
-    });
-    if (id === this.state.onClick) {
-      this.setState({ isOpen: false });
+    if (this.state.onClick === id) {
+      this.setState({ onClick: null });
+    } else {
+      this.setState({
+        onClick: id
+      });
     }
   }
 
   render() {
-    const { onClick, isOpen } = this.state;
-    const itemList = accordionData.map(item => {
+    const { onClick } = this.state;
+    const itemList = this.props.accordionData.map(item => {
       return (
-        <div key={item.id} className="item" onClick={this.handleClick}>
-          <div data-id={item.id} className="topic-name">{item.name}</div>
-          { isOpen && onClick === item.id && <div className="topic-content">{item.content}</div>}
+        <div
+          key={item.id}
+          className="item"
+          onClick={this.handleClick}>
+          <div
+            data-id={item.id}
+            className="topic-name">
+              {item.name}
+          </div>
+          {onClick === item.id &&
+          <div className="topic-content">{item.content}</div>}
         </div>
       );
     });
@@ -37,31 +44,3 @@ export default class App extends React.Component {
     );
   }
 }
-
-const accordionData = [
-  {
-    id: '1',
-    name: 'Hypertext Markup Language',
-    content: `Hypertext Markup Language (HTML) is the standard markup
-              language for creating web pages and wev applications. With
-              Cascading Style Sheets (CSS) and JavaScript, it forms a
-              triad of cornerstone technologies for the World Wide Web.`
-  },
-  {
-    id: '2',
-    name: 'Cascading Style Sheets',
-    content: `Cascading Style Sheets (CSS) is a style sheet language used
-              for describing the presentation of a document written in a
-              markup language like HTML. CSS is a cornerstone technology
-              of the World Wide Web alongside HTML and JavaScript.`
-  },
-  {
-    id: '3',
-    name: 'JavaScript',
-    content: `JavaScript often abbreviated as JS, is a high-level,
-              interpreted programing language that conforms to the
-              ECMAScript specification. JavaScript has curly-bracket
-              syntax, dynamic typing, prototype-based object-oriented,
-              and first-class functions.`
-  }
-];
--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -5,4 +5,32 @@ import App from './accordion';
 const container = document.querySelector('#root');
 const root = ReactDOM.createRoot(container);
 
-root.render(<App />);
+const accordionData = [
+  {
+    id: '1',
+    name: 'Hypertext Markup Language',
+    content: `Hypertext Markup Language (HTML) is the standard markup
+              language for creating web pages and wev applications. With
+              Cascading Style Sheets (CSS) and JavaScript, it forms a
+              triad of cornerstone technologies for the World Wide Web.`
+  },
+  {
+    id: '2',
+    name: 'Cascading Style Sheets',
+    content: `Cascading Style Sheets (CSS) is a style sheet language used
+              for describing the presentation of a document written in a
+              markup language like HTML. CSS is a cornerstone technology
+              of the World Wide Web alongside HTML and JavaScript.`
+  },
+  {
+    id: '3',
+    name: 'JavaScript',
+    content: `JavaScript often abbreviated as JS, is a high-level,
+              interpreted programing language that conforms to the
+              ECMAScript specification. JavaScript has curly-bracket
+              syntax, dynamic typing, prototype-based object-oriented,
+              and first-class functions.`
+  }
+];
+
+root.render(<App accordionData={accordionData} />);
